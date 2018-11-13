50dd8f6d002cda042840ad604b007206cc45244e
Mariela Justiniano <marielajustiniano@Marielas-MacBook.local>
2018-11-14 00:28:49 +0100

    watch button  text

---
+++ src/pages/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+import Layout from '../components/layout'
+import Image from '../components/image'
+
+const IndexPage = () => (
+  <Layout>
+    <h1>Data visualization project</h1>
+    <p>This will be the site for my final  project.</p>
+    <div style={{ maxWidth: '300px', marginBottom: '1.45rem' }}>
+      <Image />
+    </div>
+    <Link to="/page-2/">Watch now this video</Link>
+  </Layout>
+)
+
+export default IndexPage
