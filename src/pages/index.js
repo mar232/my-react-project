7e12581bba8fffbc0901a289b774fd9e9fd19141
Mariela Justiniano <marielajustiniano@Marielas-MacBook.local>
2018-11-14 00:38:45 +0100

    watch video text

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,7 +11,7 @@ const IndexPage = () => (
     <div style={{ maxWidth: '300px', marginBottom: '1.45rem' }}>
       <Image />
     </div>
-    <Link to="/page-2/">Watch now this video</Link>
+    <Link to="/page-2/">Watch this video</Link>
   </Layout>
 )
 
